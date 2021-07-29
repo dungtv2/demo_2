6f72ad911819265ce9cf1e9c51ab28e1f41f5076
Truong <truongdung.vd@gmail.com>
2021-07-29 17:21:54 +0700

    NT

---
+++ demo_2/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Dashboard Builder",
+
+    'summary': """
+      Dashboard Builder""",
+
+    'description': """
+	- Sticky Header
+	- List Sticky Header
+	- Header Sticky
+	- Fix Header
+	- Header Fix
+	- Table Header Fix
+	- Table List Fix 
+	- Table Hedar Sticky	
+    """,
+
+    'author': "Odoo Apps Store.",
+    'website': "https://www.ksolves.com/",
+    'license': 'LGPL-3',
+    'category': 'Tools',
+    'version': '13.0.1.0.0',
+    'images': ['static/description/main.jpg'],
+    'depends': ['base'],
+
+    'data': [
+        'views/assets.xml',
+    ],
+
+}
